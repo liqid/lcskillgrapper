6701d323b4efc3f8a5916e3232699e8dca80fcd7
Christian Matzke <c.matzke@linuxdeployer.unister.lan>
2013-08-08 16:45:29 +0200

    implemented first Table search

--- LC/GrapSkills.php
+++ LC/GrapSkills.php
@@ -1,5 +1,13 @@
 <?php
 namespace LC;
+use LC\Transfer\Skill as Skill;
+use LC\Transfer\Condition as Condition;
+use LC\Transfer\ConditionType as CType;
+use LC\Transfer\Effect as Effect;
+use LC\Transfer\EffectType as EType;
+use LC\Transfer\Tier as Tier;
+use LC\Transfer\SkillType as SType;
+
 /**
  * 
  */
@@ -12,22 +20,48 @@ class GrapSkills {
 	    libxml_use_internal_errors(true);
 		$this->page = new \DOMDocument();
         $this->test = $this->page->loadHTMLFile($URL);
-        foreach (libxml_get_errors() as $error) {
-            if ($error->level != LIBXML_ERR_WARNING) {
-                echo ($error->level == LIBXML_ERR_ERROR? 'ERROR ': 'FATAL ERROR ') . 'in ' . $error->file . '[line ' . $error->line . '/' . $error->column . ']:' . $error->message . PHP_EOL;
-            }
-        }
+        // foreach (libxml_get_errors() as $error) {
+            // if ($error->level != LIBXML_ERR_WARNING) {
+                // echo ($error->level == LIBXML_ERR_ERROR? 'ERROR ': 'FATAL ERROR ') . 'in ' . $error->file . '[line ' . $error->line . '/' . $error->column . ']:' . $error->message . PHP_EOL;
+            // }
+        // }
 	}
     
     function grap() {
         $tables = $this->page->getElementsByTagName('table');
         $one = '';
+        $test = 0;
+        $xpath = new \DOMXPath($this->page);
         foreach ($tables as $table) {
-            if ($table->hasAttribute('class')) {
-                $one = $table;
+            if (!$table->hasAttribute('class')) {
+                    // $skillId = 1;
+                    $trs = $table->firstChild->getElementsByTagName('tr');
+                    $trIndex = 0;
+                    foreach ($trs as $key => $tr) {
+                        $tds = $tr->getElementsByTagName('td');
+                        $tdIndex = 0;
+                        foreach ($tds as $td) {
+                            if ($trIndex == 0) {
+                                if ($tdIndex == 0) {
+                                    $skillImage = $td->firstChild->getAttribute('src');
+                                    var_dump($skillImage);
+                                } else {
+                                    $skillName = $xpath->query('descendant::*/text()',$td->firstChild)->item(0)->textContent;
+                                    var_dump($skillName);
+                                }
+                                
+                            }
+                            $tdIndex++;
+                        }
+                        if ($trIndex == 0) {
+                            break;
+                        }
+                    }
+                    $one = 'bla';
                 break;
             }
         }
+var_dump($test);
         return $one;
     }
 }
--- test.php
+++ test.php
@@ -1,18 +1,59 @@
 <?php
-$html = file_get_contents('http://guide.lastchaos.bplaced.net/index.php?title=Skills');
-libxml_use_internal_errors(true);
-$dom = new DOMDocument();
-$dom->preseveWhiteSpaces = false;
-$dom->loadHTML($html);
-$links = $dom->getElementsByTagName('a');
-$urls = array();
-$fp = fopen('test', 'ab');
-foreach ($links as $link) {
-	if (preg_match('/.*(l_a|ls_a|l_p|ls_p).*/', $link->getAttribute('href'))) {
-	    fwrite($fp, $link->getAttribute('href') . PHP_EOL);
-	    $urls[] = $link->getAttribute('href');
-	}
-}
+// $html = file_get_contents('http://guide.lastchaos.bplaced.net/index.php?title=Skills');
+// libxml_use_internal_errors(true);
+// $dom = new DOMDocument();
+// $dom->preseveWhiteSpaces = false;
+// $dom->loadHTML($html);
+// $links = $dom->getElementsByTagName('a');
+// $urls = array();
+// $fp = fopen('test', 'ab');
+// foreach ($links as $link) {
+	// if (preg_match('/.*(l_a|ls_a|l_p|ls_p).*/', $link->getAttribute('href'))) {
+	    // fwrite($fp, $link->getAttribute('href') . PHP_EOL);
+	    // $urls[] = $link->getAttribute('href');
+	// }
+// }
+// 
+// 
+// var_dump($urls);
 
+    require_once 'LC/Transfer/thrift/0.9.0/php/Thrift/Thrift.php';
+    require_once 'LC/Transfer/thrift/0.9.0/php/Thrift/ClassLoader/ThriftClassLoader.php';
+    
+    use Thrift\ClassLoader\ThriftClassLoader;
+    
+    if (!isset($GEN_DIR)) {
+        $GEN_DIR = 'gen-php';
+    }
+    if (!isset($MODE)) {
+        $MODE = 'normal';
+    }
+    
+    $loader = new ThriftClassLoader();
+    $loader->registerNamespace('Thrift', './LC/Transfer/thrift/0.9.0/php/');
+    $loader->registerNamespace('LC', './');
+    $loader->register();
+    use Thrift\Protocol\TProtocol;
+    use Thrift\Protocol\TJSONProtocol;
+    use Thrift\Transport\THttpClient;
+    use Thrift\Transport\TPhpStream;
+    use Thrift\Transport\TBufferedTransport;
+    use Thrift\Exception\TException;
+    
+    
+use LC\Transfer\SkillsClient as SkillClient;
+$socket = new THttpClient('localhost',8888,'/index.php');
 
-var_dump($urls);
+$transport = new TBufferedTransport($socket, 1024, 1024);
+$protocol = new TJSONProtocol($transport);
+
+$client = new SkillClient($protocol);
+
+$transport->open();
+
+//$test = $client->getId('bla');
+
+$huhu = new LC\GrapSkills('http://guide.lastchaos.bplaced.net/index.php?title=Kriegsmeister_Skills_aktiv');
+
+var_dump($huhu->grap());
+$transport->close();
